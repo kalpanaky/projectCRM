f3de6aa0d972c8dbcbb9b81dfbbf1fa53958bf1f
Kalpana Yadav <kalpana.yadav@gmail.com>
2017-12-04 13:34:22 -0600

    minor changes

--- src/main/java/com/crm/qa/pages/ContactsPage.java
+++ src/main/java/com/crm/qa/pages/ContactsPage.java
@@ -1,6 +1,9 @@
 package com.crm.qa.pages;
 
+/***************** auther :Kalpana*********************/
+
 import org.openqa.selenium.By;
+
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
